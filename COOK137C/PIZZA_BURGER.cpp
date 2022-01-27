47e79a0ad72db87caa58fb7abbcaa7c3d5e18910
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-01-27 20:37:48 +0530

    Create PIZZA_BURGER.cpp

---
+++ COOK137C/PIZZA_BURGER.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t = 0, x = 0, y = 0, z = 0;
+    
+    cin>>t;
+    while (t--)
+    {
+        cin>>x>>y>>z;
+        if (x >= y)
+            cout<<"PIZZA"<<endl;
+        else if (x >= z)
+            cout<<"BURGER"<<endl;
+        else
+            cout<<"NOTHING"<<endl;
+    }
+	return 0;
+}

a9618f5867c9bf6a977a7b32519b4b8ba6cbea55
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-01-27 22:11:57 +0530

    Create AVGOF3.cpp

---
+++ START23C/AVGOF3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t = 0, x = 0;
+	
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;   
+	    cout<<x-1<<" "<<x<<" "<<x+1<<endl;
+	}
+	return 0;
+}
